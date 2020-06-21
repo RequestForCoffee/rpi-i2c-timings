07eae4d483f57cda1f2253d1ae64cf9c17789917
RequestForCoffee <requestforcoffee@gmail.com>
2020-06-21 15:56:41 +0200

    Debug-print current I2C register values

--- rpi_i2c.cpp
+++ rpi_i2c.cpp
@@ -8,6 +8,8 @@
 
 #include "absl/flags/flag.h"
 #include "absl/time/time.h"
+#include "absl/strings/str_cat.h"
+#include "absl/strings/string_view.h"
 
 // Register layoout is defined in the BCM2711 ARM Peripherals Manual, section 3.2.
 //
@@ -31,6 +33,25 @@ struct I2CRegisterSet {
   uint32_t CLKT;
 };
 
+// For debugging only.
+void print_register(std::ostream& stream, absl::string_view name,
+                    uint32_t value) {
+  stream << std::left << std::setw(6) << absl::StrCat(name, ":") << "0x";
+  stream << std::hex << std::right << std::setfill('0') << std::setw(8)
+         << value << std::dec << std::setfill(' ') << std::endl;
+}
+
+std::ostream& operator<<(std::ostream& stream, const I2CRegisterSet& i2c_reg) {
+  print_register(stream, "C", i2c_reg.C);
+  print_register(stream, "S", i2c_reg.S);
+  print_register(stream, "DLEN", i2c_reg.DLEN);
+  print_register(stream, "A", i2c_reg.A);
+  print_register(stream, "FIFO", i2c_reg.FIFO);
+  print_register(stream, "DIV", i2c_reg.DIV);
+  print_register(stream, "DEL", i2c_reg.DEL);
+  print_register(stream, "CLKT", i2c_reg.CLKT);
+}
+
 ABSL_FLAG(uint32_t, set_freq_hz, 0, "I2C frequency to set in Hz");
 ABSL_FLAG(absl::Duration, set_timeout, absl::Milliseconds(35),
           "I2C clock stretch timeout to set");
@@ -51,22 +72,30 @@ int main(int argc, char** argv) {
     return errno;
   }
 
-  //I2CRegisterSet i2c_1;
   off_t offset = peripheral_addr_base + I2C1_OFFSET;
   std::cout << "ARM Linux physical address of the BSC1 controller: 0x"
             << std::hex << offset << std::dec << std::endl;
-  void* addr = mmap(nullptr, sizeof(I2CRegisterSet), PROT_READ|PROT_WRITE,
-                    MAP_SHARED, devmem_fd, offset);
+
+  auto* i2c_1 = reinterpret_cast<I2CRegisterSet*>(mmap(
+    nullptr,
+    sizeof(I2CRegisterSet),
+    PROT_READ|PROT_WRITE,
+    MAP_SHARED,
+    devmem_fd,
+    offset)
+  );
 
   // mmap(2): "After the mmap() call has returned, the file descriptor, fd,
   // can be closed immediately without invalidating the mapping."
   close(devmem_fd);
 
-  if (addr == MAP_FAILED) {
+  if (i2c_1 == MAP_FAILED) {
     perror("Could not mmap I2C registers");
     return errno;
   }
 
-  munmap(addr, sizeof(I2CRegisterSet));
+  std::cout << "I2C register state: " << std::endl << *i2c_1;
+
+  munmap(i2c_1, sizeof(I2CRegisterSet));
   return 0;
 }
